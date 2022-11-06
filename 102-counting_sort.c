137b8031befb02edb25a696277505be569e1146c
Abrham Endale <tmailae2020@gmail.com>
2022-11-06 10:42:20 -0800

    Added counting sort.

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,13 +1,36 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array using bubble sort algorithm
+ * counting_sort - Sorts an array using bubble sort algorithm
  *
  * @array: Array to be sorted
  * @size: The size of the array
  *
  */
-void bubble_sort(int *array, size_t size)
+void counting_sort(int *array, size_t size)
 {
+	unsigned int i;
+	int max = 0, *carray;
+	int *sarray = malloc (sizeof(int) * size);
 
+	for (i = 0 ; i < size ; i++)
+	{
+		if (array[i] > max)
+			max = array[i];
+	}
+	carray = malloc(sizeof(int) * (max + 1));
+	for (i = 0 ; i < size ; i++)
+		carray[array[i]]++;
+	for (i = 1 ; i < (unsigned int)max + 1 ; i++)
+		carray[i] = carray[i] + carray[i - 1];
+	print_array(carray, max + 1);
+	for (i = 0 ; i < size ; i++)
+	{
+		 sarray[carray[array[i]] - 1] = array[i];
+		 carray[array[i]]--;
+	}
+	for (i = 0 ; i < size ; i++)
+		array[i] = sarray[i];
+	free(sarray);
+	free(carray);
 }
