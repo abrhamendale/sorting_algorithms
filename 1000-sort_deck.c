1b5d1c933bd281f6f50463f30b3c1b2951081364
Abrham Endale <tmailae2020@gmail.com>
2022-11-08 13:47:49 -0800

    Added deck files.

--- 1000-sort_deck.c
+++ 1000-sort_deck.c
@@ -1,13 +1,95 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array using bubble sort algorithm
+ * swapper - Swaps array elements
+ *
+ * @array: Input array
+ * @i1: Index 1
+ * @i2: Index 2
+ *
+ * Return: Nothing
+ */
+void swapper(int *array, unsigned int i1, unsigned int i2)
+{
+	int tmp;
+
+	tmp = array[i1];
+	array[i1] = array[i2];
+	array[i2] = tmp;
+}
+/**
+ * quick_sort - Sorts an array using bubble sort algorithm
  *
  * @array: Array to be sorted
  * @size: The size of the array
  *
+ * Return: Nothing
  */
-void bubble_sort(int *array, size_t size)
+void quick_sort(int *array, size_t size)
 {
+	int pivot;
+	unsigned int i, start = 0, end = size - 1, s = -1;
 
+	/*printf("---------quick_sort---------\n");*/
+	pivot = array[size - 1];
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] < pivot)
+		{
+			s++;
+			swapper(array, i, s);
+			if (array[i] != array[s])
+				print_array(array, size);
+		}
+		if (i == size - 2)
+		{
+			swapper(array, size - 1, s + 1);
+			if (array[size - 1] != array[s + 1])
+				print_array(array, size);
+		}
+	}
+	if (s + 1 != 0)
+		partition_left(array, start, s, size);
+	partition_left(array, s + 2, end, size);
+}
+/**
+ * partition_left - partitions the array
+ *
+ * @array: Input array
+ * @start: Start of the partition
+ * @end: The end of the partition
+ * @size: The end of the partiotn
+ *
+ * Return: Nothing
+ */
+void partition_left(int *array, unsigned int start, unsigned
+		int end, size_t size)
+{
+	int pivot;
+	unsigned int i, s = -1;
+
+	pivot = array[end];
+
+	if (start == end)
+		return;
+	for (i = start; i < end; i++)
+	{
+		if (array[i] < pivot)
+		{
+			s++;
+			swapper(array, i, start + s);
+			if (array[i] != array[start + s])
+				print_array(array, size);
+		}
+		if (i == end - 1)
+		{
+			swapper(array, end, start + s + 1);
+			if (array[end] != array[start + s + 1])
+				print_array(array, size);
+		}
+	}
+	if (s + 1 > 0)
+		partition_left(array, start, start + s, size);
+	if (start + s + 1 < end)
+		partition_left(array, start + s + 2, end, size);
 }
---
+++ deck.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdarg.h>
+#include <ctype.h>
+#include <limits.h>
+#include <unistd.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void swap_elements(int *array, unsigned int index);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+listint_t *swap_list_elements(listint_t **list, listint_t *swp);
+listint_t *create_listint(const int *array, size_t size);
+size_t get_size(listint_t **list);
+void partition_left(int *array, unsigned int start, unsigned int end, size_t size);
+void swapper(int *array, unsigned int i1, unsigned int i2);
+void sorter(int *array, int i, int end);
